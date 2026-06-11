81fe733eab542a1519fffab7ffda58b2da3b8161
laggr-ctrl <ahuny12@gmail.com>
2026-06-11 13:21:00 +0800

    Add on-device AI assistant

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "focuslock-v1";
-const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+const CACHE = "focuslock-v2";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./wllama.wasm"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
