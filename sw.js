3f83da3f211d550f359218ec6f1acc472e4d3b54
laggr-ctrl <ahuny12@gmail.com>
2026-06-12 07:00:57 +0800

    Use clear default system voice for TTS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "focuslock-v11";
+const CACHE = "focuslock-v12";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./wllama.wasm"];
 
 self.addEventListener("install", (e) => {
