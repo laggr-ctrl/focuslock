c192d46cb21ea1b39d6a01bd3dcbec027c6c7fd0
laggr-ctrl <ahuny12@gmail.com>
2026-06-12 07:53:00 +0800

    Workout library + exercise animations + Health logging

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "focuslock-v16";
+const CACHE = "focuslock-v17";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./wllama.wasm"];
 
 self.addEventListener("install", (e) => {
