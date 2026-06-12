347ef50db4218da6a91a1a22225b82d98aa6fa12
laggr-ctrl <ahuny12@gmail.com>
2026-06-12 08:41:51 +0800

    Training checklist mode + workout scheduling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "focuslock-v19";
+const CACHE = "focuslock-v20";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png", "./wllama.wasm"];
 
 self.addEventListener("install", (e) => {
